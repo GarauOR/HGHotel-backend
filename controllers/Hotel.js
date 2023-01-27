45060577e74926365e569e7880911a9ca14c76fe
GarauOR <luca91garau@gmail.com>
2023-01-27 14:40:18 +0000

    build: added booking collection and calls

--- controllers/Hotel.js
+++ controllers/Hotel.js
@@ -2,6 +2,7 @@
 
 const axios = require("axios");
 const roomModel = require("../models/Hotel");
+const bookingModel = require("../models/Booking");
 const API_KEY = process.env.API_KEY;
 
 function homeHandler(req, res) {
@@ -15,7 +16,7 @@ async function getRoomsHandler(req, res) {
 
 async function getWeatherHandler(req, res) {
   const weatherRes = await axios.get(
-    `http://api.openweathermap.org/data/2.5/weather?q=York,units=metric&uk&APPID=${API_KEY}`
+    `http://api.openweathermap.org/data/2.5/weather?q=Adeje,Spain&units=metric&uk&APPID=${API_KEY}`
   );
   res.status(200).send(weatherRes.data);
 }
@@ -34,6 +35,17 @@ async function updRoomHandler(req, res) {
 
   await roomModel.findByIdAndUpdate(id, {
     dates
+  });
+  let allRooms = await roomModel.find({});
+  res.status(200).send(allRooms);
+}
+
+async function adminUpdRoomHandler(req, res) {
+  const { id } = req.params;
+  let { number, description, price } = req.body;
+
+  await roomModel.findByIdAndUpdate(id, {
+    number, description, price
   });
   let allRooms = await roomModel.find({});
   res.status(200).send(allRooms);
@@ -69,6 +81,34 @@ async function addRoomHandler(req, res) {
   res.status(200).send(allRooms);
 }
 
+async function getBookingHandler(req, res) {
+  const username = req.query.username;
+  let allRoomsData = await bookingModel.find({username:username});
+  res.status(200).send(allRoomsData);
+}
+
+async function delBookingHandler(req, res) {
+  const { id } = req.params;
+  let username = req.query.username;
+  await bookingModel.findByIdAndDelete(id);
+
+  let allRooms = await bookingModel.find({username:username});
+  res.status(200).send(allRooms);
+}
+
+async function addBookingHandler(req, res) {
+  let { description, checkin, checkout, username } = req.body;
+  await bookingModel.create({
+    description,
+    checkin,
+    checkout,
+    username
+  });
+
+  let allRooms = await bookingModel.find({username:username});
+  res.status(200).send(allRooms);
+}
+
 module.exports = {
   homeHandler,
   getWeatherHandler,
@@ -76,4 +116,8 @@ module.exports = {
   delRoomHandler,
   updRoomHandler,
   addRoomHandler,
+  adminUpdRoomHandler,
+  delBookingHandler,
+  getBookingHandler,
+  addBookingHandler,
 };
--- routes/Hotel.js
+++ routes/Hotel.js
@@ -10,5 +10,8 @@ HGRouter.get("/room", HGHandlers.getRoomsHandler);
 HGRouter.post("/room", HGHandlers.addRoomHandler);
 HGRouter.delete("/room/:id", HGHandlers.delRoomHandler);
 HGRouter.put("/room/:id", HGHandlers.updRoomHandler);
-
+HGRouter.put("/adminroom/:id", HGHandlers.adminUpdRoomHandler);
+HGRouter.get("/booking", HGHandlers.getBookingHandler);
+HGRouter.post("/booking", HGHandlers.addBookingHandler);
+HGRouter.delete("/booking/:id", HGHandlers.delBookingHandler);
 module.exports = HGRouter; 
